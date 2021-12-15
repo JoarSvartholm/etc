bfa8b2501bf30014f471fcdbf46b445fb41e94f4
Joar Svartholm <joar.svartholm@chromaway.com>
2021-12-15 12:45:43 +0100

    Automatically add zshrc file upon setup

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ brew install --cask iterm2
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source $(pwd)/.myzshrc" >> ~/.zshrc
 
 # Install Editors
 brew install --cask intellij-idea-ce
