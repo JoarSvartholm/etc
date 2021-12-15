e4c661220ac4dd6e1c31d5a656487959dcd25afa
Joar Svartholm <joar.svartholm@chromaway.com>
2021-12-15 12:37:38 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,39 @@
+xcode-select --install
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Install Terminal Options
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --cask iterm2
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install Editors
+brew install --cask intellij-idea-ce
+brew install --cask pycharm-ce
+
+
+#Install Tech Stack
+brew install AdoptOpenJDK/openjdk/adoptopenjdk{8,11}
+brew install jenv maven kotlin docker
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
+jenv add /usr/local/Cellar/openjdk/17.0.1/libexec/openjdk.jdk/Contents/Home
+# brew install openjdk@8
+# sudo ln -sfn /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-8.jdk
+# brew install openjdk@11
+# sudo ln -sfn /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+# brew install openjdk@17
+# sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+#Db
+brew install postgresql
+
+# Communication
+brew install --cask microsoft-teams
+brew install --cask zoom
+
+# Install Misc
+brew install --cask opera
+brew install --cask spotify
+brew install --cask spectacle
